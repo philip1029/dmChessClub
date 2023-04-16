1240792c3edbdb3b3e3572961592133ac6bde879
philip1029 <110770558+philip1029@users.noreply.github.com>
2023-04-16 10:33:30 +0900

    v1.0.0-beta
    사이트 생성

---
+++ js/dcc.js
@@ -0,0 +1,6 @@
+function h1(str) {
+    document.write('<summary><b style="font-size: 200%;">' + str + '</b></summary>')
+}
+function h2(str) {
+    document.write('<summary><b style="font-size: 150%;">' + str + '</b></summary>')
+}
